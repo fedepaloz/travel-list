d8250829884527f464f809a43386d7d4d0640884
fedepaloz <palozzifederico@gmail.com>
2023-09-12 11:36:53 +0200

    layout + rendering list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,57 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Paje", quantity: 20, packed: false },
+];
+
 function App() {
-  return <div>ok</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>far away</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>what do you need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button>X</button>
+    </li>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>you have x items on the list and you packed x</em>
+    </footer>
+  );
 }
 
 export default App;
