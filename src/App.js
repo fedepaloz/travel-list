9ffac45f3afb8492e448e9c49b3ff20b8c743b10
fedepaloz <palozzifederico@gmail.com>
2023-09-13 10:39:52 +0200

    form completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Paje", quantity: 20, packed: false },
+  { id: 3, description: "Paje", quantity: 20, packed: false },
 ];
 
 function App() {
@@ -19,10 +21,35 @@ function Logo() {
   return <h1>far away</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleSubmit(e) {
+    if (description === "") {
+      alert("inserisci descrizione");
+    } else e.preventDefault();
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do you need for your trip?</h3>
-    </div>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
@@ -30,7 +57,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
